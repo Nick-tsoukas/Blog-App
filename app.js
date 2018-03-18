a3bbf5bdbe819b13cbb46d99a4f6705cd83e91e5
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-03-17 21:02:06 -0600

    Adding sanitizer module

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var bodyParser = require("body-parser"),
+expressSanitizer = require("express-sanitizer"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
+moment         = require("moment")
 emoji          = require("node-emoji"),
 app            = express();
 
@@ -8,12 +11,14 @@ mongoose.connect("mongodb://localhost/rest_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  created: {type: Date, default: Date.now}
+  created: {type: Date, default: new Date(Date.now())}
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
@@ -48,6 +53,9 @@ app.get('/blogs/new', function(req,res) {
 //Create route post request and then redirect
 app.post("/blogs", function(req,res) {
   //create blogs
+  console.log(req.body)
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  console.log(req.body)
   Blog.create(req.body.blog, function(err, newBlog){
     if(err) {
       res.render("new")
@@ -58,12 +66,53 @@ app.post("/blogs", function(req,res) {
 
 });
 
+
+// SHOW ROUTE
 app.get("/blogs/:id", function(req,res){
   Blog.findById(req.params.id, function(err, foundBlog){
     if(err){
       res.redirect("/blogs");
     } else {
       res.render("show",{blog:foundBlog})
+    }
+  });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req,res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs")
+    } else {
+      res.render("edit", {blog : foundBlog})
+    }
+  });
+})
+
+// UPDATE Routes
+app.put("/blogs/:id/", function(req, res) {
+  // second parameter is equal to the value of name attribute in FORM === req.body.blog
+  // findByIdAndUpdate method will update database !important
+
+  //using sanitize module to remove scripts from the blog body
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id)
+    }
+  });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+  //destroy blog and redirect
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
     }
   });
 });
